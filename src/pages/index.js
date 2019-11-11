cec440165c5e13a0632ce6626dca38896829e256
20641118 <callehj@hotmail.se>
2019-11-11 14:12:19 +0100

    Add netlify cms?

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,17 @@ import SEO from "../components/seo";
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Webbutveckling i Mariestad" />
-		
+		<section className="hero frontpage">
+			<div className="content">
+				<div className="copy">
+					<h1>Jag utvecklar hemsidor</h1>
+					<p>Jag skapar moderna och snabba hemsidor till ett ovanligt lågt pris.</p>
+					<div className="btns">
+						
+					</div>
+				</div>
+			</div>
+		</section>
 	</Layout>
 );
 
