0b49cd967e92cccdce96e83c7a5adfe29b66e96d
20641118 <callehj@hotmail.se>
2019-11-11 18:16:21 +0100

    Add link to header logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "gatsby";
 
 import LogoIcon from "../images/logotyp.svg";
 
 export default function Logo({ siteTitle }) {
 	return (
-		<img className="logo" src={LogoIcon} alt={siteTitle} />
+		<Link to="/">
+			<img className="logo" src={LogoIcon} alt={siteTitle} />
+		</Link>
+			
 	)
 }
