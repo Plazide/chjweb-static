83ebde37e9a70efb1dbf8bdf449fd0837305d432
20641118 <callehj@hotmail.se>
2019-11-12 16:18:34 +0100

    Create slug field and post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,56 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path");
+const { fmImagesToRelative } = require("gatsby-remark-relative-images");
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+	const { createNodeField } = actions;
+	fmImagesToRelative(node);
+	if(node.internal.type === "MarkdownRemark"){
+		const value = createSlug({ node });
+		createNodeField({
+			name: "slug",
+			node,
+			value
+		});
+	}
+}
+
+exports.createPages = async ({ actions, graphql }) => {
+	const {createPage} = actions;
+
+	const result = await graphql(`
+	{
+		allMarkdownRemark{
+			edges{
+				node{
+					fields{
+						slug
+					}
+				}
+			}
+		}
+	}`);
+	const posts = result.data.allMarkdownRemark.edges;
+
+	posts.forEach( ({node}) => {
+		createPage({
+			path: `/blogg/${node.fields.slug}`,
+			component: path.resolve("./src/templates/post.js"),
+			context: {
+				slug: node.fields.slug
+			}
+		})
+	});
+
+	return posts;
+}
+
+function createSlug({ node }){
+	const filePath = node.fileAbsolutePath;
+	const parts = filePath.split("/");
+	const slug = parts[parts.length - 1].split(".")[0];
+
+	return slug;
+}
