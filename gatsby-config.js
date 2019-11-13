6a5350010d1a4c798f72ed396aee61c4629ceb6a
20641118 <callehj@hotmail.se>
2019-11-13 12:16:44 +0100

    Configure rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,65 @@ module.exports = {
 		title: "CHJ Webblösningar",
 		description: "Skaffa dig en modern, väl presterande och säker hemsida med CHJ Webblösningar.",
 		author: "CHJ Webblösningar",
+		siteUrl: "https://www.chjweb.se",
 		lang: "sv"
 	},
 	plugins: [
 		"gatsby-plugin-react-helmet",
+		{
+			resolve: "gatsby-plugin-feed",
+			options: {
+				query: `
+				{
+					site {
+						siteMetadata{
+							title
+							description
+							siteUrl
+							site_url: siteUrl
+						}
+					}
+				}
+				`,
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark} }) => {
+							return allMarkdownRemark.edges.map( edge => {
+								return Object.assign({}, edge.node.frontmatter, {
+									description: edge.node.frontmatter.description,
+									date: edge.node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + "/" + edge.node.fields.slug,
+									guid: site.siteMetadata.siteUrl + "/" + edge.node.fields.slug,
+									custom_elements: [{ "content:encoded": edge.node.html }]
+								})
+							})
+						},
+						query: `
+						{
+							allMarkdownRemark( sort: { order: DESC, fields: [frontmatter___date] } ){
+								edges {
+									node{
+										frontmatter{
+											description
+											title
+											date
+										}
+										fields {
+											slug
+										}
+										html
+									}
+								}
+							}
+						}
+						`,
+						output: "/rss.xml",
+						title: "CHJ Webblösningar RSS Feed",
+						match: "^\/blogg\/.*$"
+					}
+				]
+			}
+		},
 		{
 			resolve: "gatsby-source-filesystem",
 			options: {
