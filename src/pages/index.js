c9ce7030badcdeeff92b23626b61864b238b84ef
20641118 <callehj@hotmail.se>
2019-11-13 14:09:25 +0100

    Add featured item above page heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,20 @@ import ButtonLink from "../components/ButtonLink";
 // Images
 import illustrationUrl, { ReactComponent as Illustration} from "../images/illustrations/create.svg";
 import waveUrl, { ReactComponent as Wave } from "../images/illustrations/wave.svg";
+import arrowUrl, { ReactComponent as Arrow } from "../images/illustrations/arrow.svg";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
 	<Layout>
 		<SEO title="Webbutveckling i Mariestad" />
 		<section className="hero frontpage">
 			<Wave className="background" />
 			<div className="content">
 				<div className="copy">
+					<a href={`/blogg/${data.allMarkdownRemark.nodes[0].fields.slug}`} className="featured">
+						<span>Senaste inlägget: </span>
+						<span className="title">{data.allMarkdownRemark.nodes[0].frontmatter.title}</span>
+						<Arrow />
+					</a>
 					<h1>Jag skapar hemsidor</h1>
 					<p>Jag skapar moderna, snygga, snabba och unika hemsidor med hjälp av den senaste tekniken inom webbutveckling.</p>
 					<div className="btns">
@@ -37,5 +43,20 @@ const IndexPage = () => (
 		</section>
 	</Layout>
 );
+
+export const pageQuery = graphql`
+query FeaturedQuery {
+	allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}){
+		nodes {
+			fields {
+				slug
+			}
+			frontmatter {
+				title
+			}
+		}
+	}
+}
+`
 
 export default IndexPage;
