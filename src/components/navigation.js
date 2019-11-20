801b56495b7f2a86dbd8a31933c0951795e05c5e
20641118 <callehj@hotmail.se>
2019-11-20 17:17:46 +0100

    Add active class

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,11 +4,36 @@ import { Link } from "gatsby"
 export default function navigation() {
 	return (
 		<nav>
-			<Link to="/">Hem</Link>
-			<Link to="/hosting">Hosting</Link>
-			<Link to="/webbyra">Webbyrå</Link>
-			<Link to="/cms">CMS</Link>
-			<Link to="/blogg">Blogg</Link>
+			<Link 
+				to="/" 
+				activeClassName="active">
+					Hem
+			</Link>
+
+			<Link 
+				to="/hosting" 
+				activeClassName="active">
+					Hosting
+			</Link>
+
+			<Link 
+				to="/webbyra"
+				activeClassName="active">
+					Webbyrå
+			</Link>
+
+			<Link 
+				to="/cms"
+				activeClassName="active">
+					CMS
+			</Link>
+
+			<Link 
+				to="/blogg"
+				activeClassName="active"
+				partiallyActive={true}>
+					Blogg
+			</Link>
 		</nav>
 	)
 }
