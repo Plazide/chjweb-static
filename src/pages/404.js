fac8c59ec7236cc0f26171e499c2c7e7c7c98bdb
20641118 <callehj@hotmail.se>
2019-11-21 08:24:30 +0100

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,20 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+// Css
+import "../styles/404.css";
+
 const NotFoundPage = () => (
 	<Layout>
-		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<SEO title="Sidan hittades inte" />
+		<section className="not-found">
+			<div className="content">
+				<a href="/">Tillbaka</a>
+				<h1>Sidan kunde inte hittas!</h1>
+				<span className="code">404</span>
+			</div>
+		</section>
+		
 	</Layout>
 );
 
