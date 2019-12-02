fd639f486099173b7f6e845b1cad9a87418b2410
20641118 <callehj@hotmail.se>
2019-12-02 10:13:29 +0100

    Add restructure navigation with dropdown links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,39 +1,53 @@
 import React from 'react'
 import { Link } from "gatsby"
+import DropdownLink from './DropdownLink'
+import ButtonLink from "./ButtonLink";
 
 export default function navigation() {
 	return (
 		<nav>
 			<Link 
 				to="/" 
-				activeClassName="active">
-					Hem
+				activeClassName="active"
+			>
+				Hem
 			</Link>
 
-			<Link 
-				to="/hosting" 
-				activeClassName="active">
-					Hosting
-			</Link>
+			<DropdownLink
+				to="/tjanster"
+				activeClassName="active"
+				partiallyActive={true}
+				subLinks={[
+					{ name: "E-post", to: "/tjanster/email-hosting" },
+					{ name: "Granskning", to: "/tjanster/granskning" },
+					{ name: "Utveckling", to: "/webbyra" }
+				]}
+			>
+				Tjänster
+			</DropdownLink>
 
-			<Link 
+			<DropdownLink
 				to="/webbyra"
-				activeClassName="active">
-					Webbyrå
-			</Link>
-
-			<Link 
-				to="/cms"
-				activeClassName="active">
-					CMS
-			</Link>
+				activeClassName="active"
+				partiallyActive={true}
+				subLinks={[
+					{ name: "SEO", to: "/webbyra/seo" }, 
+					{ name: "Hosting", to:"/webbyra/hosting" },
+					{ name: "CMS", to: "/webbyra/cms" }
+				]}
+			>
+				Webbyrå
+			</DropdownLink>
 
 			<Link 
 				to="/blogg"
 				activeClassName="active"
-				partiallyActive={true}>
-					Blogg
+				partiallyActive={true}
+			>
+				Blogg
 			</Link>
+
+			<ButtonLink variant="outlined" href="/anlita">Anlita mig</ButtonLink>
 		</nav>
 	)
 }
