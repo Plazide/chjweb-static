b72d75a17d40c29eeefdefb40928769b80b04d1b
20641118 <callehj@hotmail.se>
2019-12-02 10:15:02 +0100

    Add option for adding cta and allow illustration to be either string or component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function Hero({ title, children, illustration }) {
+export default function Hero({ title, children, illustration, cta = "" }) {
 	return (
 		<section className="hero">
 			<div className="content">
 				<div className="copy">
 					<h1>{title}</h1>
 					<p>{children}</p>
+					{cta}
 				</div>
 				<div className="illustration">
-					{illustration}
+					{typeof illustration === "string" ? <img src={illustration} alt={title} /> : illustration}
 				</div>
 			</div>
 		</section>
