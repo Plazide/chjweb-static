8789ad38961b3f2c0ff130d5a7e5e2e40c3e09b1
20641118 <callehj@hotmail.se>
2019-12-02 10:09:20 +0100

    Make Row a shared component

---
+++ src/components/Row.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {useInView} from "react-intersection-observer";
+
+// Components
+import ButtonLink from "./ButtonLink";
+
+export default function Row({ illustration, link, align, title, children, paragraph = true, threshold = 1 }){
+	const [ref, inView] = useInView({ threshold, triggerOnce: true });
+
+	const visible = inView;
+	const translate = visible ? "0px" : "50%";
+	const opacity = visible ? "1" : "0";
+	const direction = align === "left" ? "row" : "row-reverse";
+	const alignItems = align === "left" ? "flex-start" : "flex-end";
+	const alignSelf = align === "left" ? "flex-start" : "flex-end";
+	const content = paragraph ? <p>{children}</p> : children;
+
+	const copyStyle = {
+		alignItems, 
+		textAlign: align, 
+		transform: `translate(${align === "left" ? "-" : ""}${translate})`
+	}
+
+	const button = link ? <ButtonLink href={link} variant="text" style={{ alignSelf }}>Läs mer</ButtonLink> : "";
+
+	return (
+		<section ref={ref} className="row" style={{ flexDirection: direction, opacity }}>
+			<div className="copy" style={copyStyle}>
+				<h1>{title}</h1>
+				{content}
+				{button}
+			</div>
+			<div className="illustration" role="presentation">
+				{typeof illustration === "string" ? <img src={illustration} alt={title} /> : illustration}
+			</div>
+		</section>
+	)
+}
