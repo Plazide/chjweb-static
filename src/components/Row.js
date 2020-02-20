c7de8c4444d3adcc0bdd3f68e9a9f5208e4f12b5
20641118 <callehj@hotmail.se>
2020-02-20 16:04:38 +0100

    Change default threshold

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import {useInView} from "react-intersection-observer";
+import React from "react";
+import { useInView } from "react-intersection-observer";
 
 // Components
 import ButtonLink from "./ButtonLink";
 
-export default function Row({ illustration, link, align, title, children, paragraph = true, threshold = 1 }){
-	const [ref, inView] = useInView({ threshold, triggerOnce: true });
+export default function Row ({ illustration, link, align, title, children, paragraph = true, threshold = 0.3 }){
+	const[ref, inView] = useInView({ threshold, triggerOnce: true });
 
 	const visible = inView;
 	const translate = visible ? "0px" : "50%";
@@ -16,14 +16,14 @@ export default function Row({ illustration, link, align, title, children, paragr
 	const content = paragraph ? <p>{children}</p> : children;
 
 	const copyStyle = {
-		alignItems: "flex-start", 
-		textAlign: "left", 
+		alignItems: "flex-start",
+		textAlign: "left",
 		transform: `translate(${align === "left" ? "-" : ""}${translate})`
-	}
+	};
 
 	const button = link ? <ButtonLink href={link} variant="text" style={{ alignSelf: "flex-start" }}>Läs mer</ButtonLink> : "";
 
-	return (
+	return(
 		<section ref={ref} className="row" style={{ flexDirection: direction, opacity }}>
 			<div className="copy" style={copyStyle}>
 				<h1>{title}</h1>
@@ -34,5 +34,5 @@ export default function Row({ illustration, link, align, title, children, paragr
 				{typeof illustration === "string" ? <img src={illustration} alt={title} /> : illustration}
 			</div>
 		</section>
-	)
+	);
 }
