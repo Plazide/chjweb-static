8135a88ffc558591455ef32c29b837015bc5c648
20641118 <callehj@hotmail.se>
2020-03-10 19:03:03 +0100

    Remove confusables from slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,10 +6,11 @@
 
 // You can delete this file if you're not using it
 const path = require("path");
-const { fmImagesToRelative } = require("gatsby-remark-relative-images");
+const{ fmImagesToRelative } = require("gatsby-remark-relative-images");
+const{ remove } = require("confusables");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-	const { createNodeField } = actions;
+	const{ createNodeField } = actions;
 	fmImagesToRelative(node);
 	if(node.internal.type === "MarkdownRemark"){
 		const value = createSlug({ node });
@@ -19,10 +20,10 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 			value
 		});
 	}
-}
+};
 
 exports.createPages = async ({ actions, graphql }) => {
-	const {createPage} = actions;
+	const{ createPage } = actions;
 
 	const result = await graphql(`
 	{
@@ -38,23 +39,23 @@ exports.createPages = async ({ actions, graphql }) => {
 	}`);
 	const posts = result.data.allMarkdownRemark.edges;
 
-	posts.forEach( ({node}) => {
+	posts.forEach( ({ node }) => {
 		createPage({
 			path: `/blogg/${node.fields.slug}`,
 			component: path.resolve("./src/templates/post.js"),
 			context: {
 				slug: node.fields.slug
 			}
-		})
+		});
 	});
 
 	return posts;
-}
+};
 
-function createSlug({ node }){
+function createSlug ({ node }){
 	const filePath = node.fileAbsolutePath;
 	const parts = filePath.split("/");
-	const slug = parts[parts.length - 1].split(".")[0];
+	const slug = remove(parts[parts.length - 1].split(".")[0]);
 
 	return slug;
 }
