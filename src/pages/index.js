637df82d06233727d3a1392c8c7f8bf30332431f
Carl Hallén Jansson <callehj@hotmail.se>
2020-09-22 09:42:43 +0200

    Redo landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql, Link } from "gatsby";
+import { useInView } from "react-intersection-observer";
+import Img from "gatsby-image";
 
 // Structure
 import Layout from "../components/layout";
@@ -14,15 +16,16 @@ import Row from "../components/Row";
 // Images
 import { ReactComponent as Illustration } from "../images/illustrations/create.svg";
 import { ReactComponent as Wave } from "../images/illustrations/wave.svg";
+import { ReactComponent as Checkmark } from "../images/icons/checkmark.svg";
+import { ReactComponent as Speed } from "../images/illustrations/speed.svg";
+import { ReactComponent as Secure } from "../images/illustrations/secure.svg";
 import hostingUrl from "../images/illustrations/hosting.svg";
-import granskningUrl from "../images/illustrations/granskning_front.svg";
-import circles1Url from "../images/illustrations/circles_1.svg";
-import circles2Url from "../images/illustrations/circles_2.svg";
 import workflowUrl from "../images/illustrations/workflow.svg";
 import workflowArrowUrl from "../images/illustrations/workflow_arrow.svg";
 
 const IndexPage = ({ data }) => {
 	const meta = data.site.siteMetadata;
+	const personalImage = data.personalImage.childImageSharp.fluid;
 	const structuredData = [
 		{
 			"@context": "https://schema.org",
@@ -79,23 +82,23 @@ const IndexPage = ({ data }) => {
 
 				<Row
 					illustration={hostingUrl}
-					link="/webbyra"
+					/* link="/webbyra" */
 					align="left"
 					title="Spara pengar på driften">
 					Det kan kosta att driva en webbplats, speciellt om du får många besökare. Tack vare JAMStack är detta inte längre ett problem. Du kan driva webbplatsen nästan helt gratis, oavsett hur många besökare du får.
 				</Row>
 
 				<Row
-					illustration={granskningUrl}
-					link="/tjanster/granskning#cta"
+					illustration={<Speed />}
+					/* link="/tjanster/granskning#cta" */
 					align="right"
 					title="Så snabbt som det går">
 					Dina besökare förväntar sig att webbplatsen laddar på under 2 sekunder, annars går dem någon annanstans. Detta är inget problem för JAMStack sidor. Tack vare att varje sida redan är färdig vid varje hämtning och att filerna ligger på ett CDN, laddas webbplatsen supersnabbt.
 				</Row>
 
 				<Row
-					illustration={hostingUrl}
-					link="/webbyra/hosting"
+					illustration={<Secure />}
+					/* link="/webbyra/hosting" */
 					align="left"
 					title="Undvik att bli hackad">
 					Du behöver inte längre oroa dig för att webbplatsen ska bli hackad. På en JAMStack sida finns ingen webbserver eller databas bakom webbplatsen. Det betyder att det finns färre sårbara områden som en hackare kan använda för att ställa till det.
@@ -105,6 +108,13 @@ const IndexPage = ({ data }) => {
 
 			<section className="about">
 				<Row
+					illustration={<Img fluid={personalImage} className="personal-image" />}
+					align="left"
+					title="Vem är jag?"
+				>
+					Mitt namn är Carl. Jag fann mitt intresse för webbutveckling under min tidiga tonår. Sedan dess har jag bara fortsatt lära mig nya språk och verktyg för att bygga intressanta projekt. Det har blivit en slags passion som bara växt med tiden.
+				</Row>
+				{/* <Row
 					illustration={circles1Url}
 					align="left"
 					title="Bakom CHJ Webblösningar"
@@ -118,7 +128,7 @@ const IndexPage = ({ data }) => {
 					title="Självlärd webbutvecklare"
 				>
 					Mina erfarenheter och kunskaper kommer ifrån att jag under många år testat och experimenterat mig fram genom olika projekt. Jag har skapat webbshoppar, köp- och säljsidor och presentationssidor till olika företag innan jag startade mitt företag. Jag har alltså lärt mig webbutveckling genom att skapa verkliga hemsidor som använts av riktiga människor.
-				</Row>
+				</Row> */}
 			</section>
 
 			<section className="workflow">
@@ -127,14 +137,16 @@ const IndexPage = ({ data }) => {
 					align="left"
 					title="Hur går det till?"
 					paragraph={false}
-					threshold={0.5}>
+					threshold={0.5}
+					animate={false}
+				>
 					<ol>
 						<ListItem pos="1">Vi diskuterar hemsidans innehåll, funktioner och design</ListItem>
 						<ListItem pos="2">Jag återkommer med design förslag som ni sedan ger synpunkter på</ListItem>
 						<ListItem pos="3">Jag redigerar designförslaget</ListItem>
 						<ListItem pos="4">Jag börjar bygga er hemsida utifrån det innehåll och de funktioner som vi diskuterat</ListItem>
 						<ListItem pos="5">Vi stämmer av för att se om det finns ytterligare funktioner eller innehåll som ska läggas till</ListItem>
-						<ListItem pos="6" last={true}>Jag lanserar hemsidan och skickar en faktura</ListItem>
+						<ListItem pos="6" last={true}>Vi lanserar hemsidan</ListItem>
 					</ol>
 				</Row>
 			</section>
@@ -148,17 +160,29 @@ const IndexPage = ({ data }) => {
 	);
 };
 
-function ListItem ({ children, pos, last = false }){
-	const nextArrow = !last ? <img src={workflowArrowUrl} alt="Pil ner" className="next-arrow" /> : "";
+function ListItem({ children, pos, last = false }){
+	const[ref, inView] = useInView({ triggerOnce: true, threshold: 0.3, rootMargin: "80px 0px 80px 0px" });
+	const styles = {};
+	const arrowStyles = {};
+
+	if(inView){
+		styles.transform = "translateX(0)";
+		arrowStyles.opacity = "1";
+	}
 
 	return(
-		<li>
-			<div>
-				<span className="pos">{pos}</span>
-				{nextArrow}
+		<li ref={ref}>
+			<div style={styles} className="list-content">
+				<div>
+					<span className="pos">{last ? <Checkmark className="checkmark" /> : pos}</span>
+					{
+						!last
+							? <img src={workflowArrowUrl} style={arrowStyles} alt="Pil ner" className="next-arrow" />
+							: ""
+					}
+				</div>
+				{children}
 			</div>
-			{children}
-
 		</li>
 	);
 }
@@ -203,6 +227,13 @@ query FeaturedQuery {
 			zipCode
 			phone
 			email
+		}
+	}
+	personalImage: file(relativePath: { eq: "raster/carl.png" }){
+		childImageSharp{
+			fluid(maxWidth: 500){
+				...GatsbyImageSharpFluid
+			}
 		}
 	}
 }
