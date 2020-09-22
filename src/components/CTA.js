ed48a03056cefe4f2a5054091593bf0803668855
Carl Hallén Jansson <callehj@hotmail.se>
2020-09-22 10:49:14 +0200

    Add prop types and change button

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
 // Components
-import ButtonLink from './ButtonLink';
+import ButtonLink from "./ButtonLink";
 
 // css
 import "./styles/cta.css";
 
-export default function CTA({ title, children }) {
-	return (
+export default function CTA({ title, children }){
+	return(
 		<section className="cta">
 			<div className="content">
 				<h1>{title}</h1>
 				<p>{children}</p>
 
-				<ButtonLink variant="filled" href="/offert/">Be om offert</ButtonLink>
+				<ButtonLink variant="filled" href="/kontakt">Kontakta mig</ButtonLink>
 			</div>
 		</section>
-	)
+	);
 }
+
+CTA.propTypes = {
+	title: PropTypes.string,
+	children: PropTypes.node
+};
