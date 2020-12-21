f3c8eecf6edeb265c8f755deefc0855a49f3706d
Carl Hallén Jansson <callehj@hotmail.se>
2020-12-21 18:52:30 +0100

    Change to smaller logo when screen is below 400px wide

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "gatsby";
-import { useTranslation } from "react-i18next";
+import { Link } from "gatsby-plugin-react-i18next";
+import { useMediaQuery } from "react-responsive";
 
 import LogoIcon from "../images/logotyp.svg";
+import LogoIconSmall from "../images/small.svg";
 
 export default function Logo(){
-	const{ i18n } = useTranslation("", { useSuspense: false });
-	const{ language } = i18n;
-	const prefix = language !== "sv" ? `/${language}` : "";
+	const isSmall = useMediaQuery({ query: "(max-width: 400px)" });
 
 	return(
-		<Link to={prefix + "/"}>
-			<img className="logo" src={LogoIcon} alt="CHJ Webblösningar" />
+		<Link to="/">
+			{
+				isSmall
+					? <img className="logo" src={LogoIconSmall} alt="Carl Hallén Jansson" />
+					: <img className="logo" src={LogoIcon} alt="Carl Hallén Jansson" />
+			}
+
 		</Link>
 
 	);
