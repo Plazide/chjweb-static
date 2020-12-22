a9d761ad7b3304ac7d33fb7f3f790790b247e0a6
Carl Hallén Jansson <callehj@hotmail.se>
2020-12-22 18:04:38 +0100

    Add translations to contact page

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
+import { useTranslation, Trans } from "gatsby-plugin-react-i18next";
 
 // Structure
 import Layout from "../components/layout";
@@ -19,12 +20,13 @@ import { useForm, Controller } from "react-hook-form";
 import Loader from "../components/Loader";
 
 export default function kontakt({ data, pageContext: { locale, data: localeData } }){
+	const{ t } = useTranslation("Contact", { useSuspense: false });
 	const structuredData = [
 		{
 			"@context": "https://schema.org",
 			"@type": "ContactPage",
 			"name": "Kontakta mig",
-			"description": "Ta kontakt med CHJ Webblösningar via telefon eller e-post",
+			"description": t("seo.description"),
 			"publisher": {
 				"@type": "LocalBusiness",
 				"name": "CHJ Webblösningar"
@@ -36,12 +38,12 @@ export default function kontakt({ data, pageContext: { locale, data: localeData 
 	return(
 		<Layout locale={locale} localeData={localeData}>
 			<SEO
-				title="Kontakta mig"
+				title={t("seo.title")}
 				url="/kontakt/"
-				description="Ta kontakt med CHJ Webblösningar via telefon eller e-post"
+				description={t("seo.description")}
 				breadcrumb={[
 					{
-						name: "Kontakta mig",
+						name: t("seo.title"),
 						url: "/kontakt/"
 					}
 				]}
@@ -51,12 +53,12 @@ export default function kontakt({ data, pageContext: { locale, data: localeData 
 			<section className="hero contact">
 				<div className="content">
 					<div className="copy">
-						<h1>Kontakta mig</h1>
-						<p>Har du några frågor? Är du ute efter en webbplats? Då kan du kontakta mig här!</p>
+						<h1>{t("hero.heading")}</h1>
+						<p>{t("hero.content")}</p>
 
 						<div className="buttons">
-							<a className="button button-filled" href={`mailto:${email}`}>Skicka ett mejl</a>
-							<a className="button button-outlined call" href={`tel:${parseNumber(phone)}`}>Ring mig</a>
+							<a className="button button-filled" href={`mailto:${email}`}>{t("hero.mailto")}</a>
+							<a className="button button-outlined call" href={`tel:${parseNumber(phone)}`}>{t("hero.tel")}</a>
 						</div>
 					</div>
 
@@ -68,8 +70,8 @@ export default function kontakt({ data, pageContext: { locale, data: localeData 
 			<section className="form">
 				<div className="content">
 					<div className="copy">
-						<h2>Skicka ett meddelande</h2>
-						<p>Vill du höra av dig, skicka ett meddelande här!</p>
+						<h2>{t("form.heading")}</h2>
+						<p>{t("form.content")}</p>
 					</div>
 					<Form />
 				</div>
@@ -79,6 +81,7 @@ export default function kontakt({ data, pageContext: { locale, data: localeData 
 }
 
 function Form(){
+	const{ t } = useTranslation("Contact", { useSuspense: false });
 	const{ control, errors, handleSubmit, formState } = useForm({ mode: "onChange" });
 	const[success, setSuccess] = useState(null);
 	const{ isSubmitting, isSubmitSuccessful } = formState;
@@ -102,14 +105,14 @@ function Form(){
 				<Controller
 					name="name"
 					control={control}
-					rules={{ required: "Du missade att skriva ditt namn!" }}
+					rules={{ required: t("form.name.error.empty") }}
 					render={({ onChange, name }) => (
 						<Input
-							label="Vad heter du?"
+							label={t("form.name.label")}
 							onChange={onChange}
 							name={name}
 							errorText={errors.name ? errors.name.message : ""}
-							helperText="Skriv gärna både för- och efternamn"
+							helperText={t("form.name.hint")}
 						/>
 					)}
 				/>
@@ -118,19 +121,19 @@ function Form(){
 					name="email"
 					control={control}
 					rules={{
-						required: "Du missade att skriva din e-post",
+						required: t("form.email.error.empty"),
 						pattern: {
 							value: /^.*@.*..*$/,
-							message: "Adressen ser konstig ut. Stämmer den verkligen?"
+							message: t("form.email.error.invalid")
 						}
 					}}
 					render={({ onChange, name }) => (
 						<Input
-							label="E-post"
+							label={t("form.email.label")}
 							onChange={onChange}
 							name={name}
 							errorText={errors[name] ? errors[name].message : ""}
-							helperText="En e-post jag kan nå dig på"
+							helperText={t("form.email.hint")}
 						/>
 					)}
 				/>
@@ -140,10 +143,10 @@ function Form(){
 			<Controller
 				name="message"
 				control={control}
-				rules={{ required: "Du missade att skriva ett meddelande" }}
+				rules={{ required: t("form.message.error.empty") }}
 				render={({ onChange, name }) => (
 					<Input
-						label="Meddelande"
+						label={t("form.message.label")}
 						mode="textarea"
 						name={name}
 						errorText={errors[name] ? errors[name].message : ""}
@@ -157,13 +160,25 @@ function Form(){
 					? <Loader />
 					: isSubmitSuccessful
 						? (success
-							? <div className="success"><b>Ditt meddelande har skickats!</b> Jag hör av mig så snart som möjligt.</div>
+							? <div className="success">
+								<Trans
+									i18nKey="form.success"
+									t={t}
+									components={{ bold: <strong /> }}
+								/>
+							</div>
 							: <>
-								<button className="button button-filled" disabled={!canSubmit}>Skicka</button>
-								<div className="failure"><b>Kunde inte skicka!</b> Något gick fel, försök igen om en liten stund.</div>
+								<button className="button button-filled" disabled={!canSubmit}>{t("form.submit")}</button>
+								<div className="failure">
+									<Trans
+										i18nKey="form.failure"
+										t={t}
+										components={{ bold: <strong /> }}
+									/>
+								</div>
 							</>
 						)
-						: <button className="button button-filled" disabled={!canSubmit}>Skicka</button>
+						: <button className="button button-filled" disabled={!canSubmit}>{t("form.submit")}</button>
 			}
 
 		</form>
