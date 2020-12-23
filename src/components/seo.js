7ea1c80971a3296c745dd8598a7f0612b09ff63f
Carl Hallén Jansson <callehj@hotmail.se>
2020-12-23 10:44:26 +0100

    Add hreflang pointers

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,14 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+import { useLocation } from "@reach/router";
 
-function SEO({ description, lang, meta, title, url, structuredData, breadcrumb, image = "/images/logotyp.png" }) {
-  const { site } = useStaticQuery(
-    graphql`
+function SEO({ description, lang, meta, title, url, structuredData, breadcrumb, image = "/images/logotyp.png" }){
+	const{ pathname } = useLocation();
+	const path = pathname.split("/").filter( path => path !== "en").join("/");
+	const{ site } = useStaticQuery(
+		graphql`
       query {
         site {
           siteMetadata {
@@ -24,112 +27,125 @@ function SEO({ description, lang, meta, title, url, structuredData, breadcrumb, 
         }
       }
     `
-  )
+	);
 
-  const metaDescription = description || site.siteMetadata.description
+	const metaDescription = description || site.siteMetadata.description;
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-	  titleTemplate={`%s | ${site.siteMetadata.title}`}
-	  link={
-		  [
-			  {
-				  rel: "canonical",
-				  href: site.siteMetadata.siteUrl + url
-			  }
-		  ]
-	  }
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-		},
-		{
-		  property: "og:url",
-		  content: site.siteMetadata.siteUrl + url
-		},
-		{
-		  property: "og:image",
-		  content: site.siteMetadata.siteUrl + image
-		},
-        {
-          property: `og:type`,
-          content: `website`,
-		},
-		{
-		  property: "twitter:image",
-		  content: site.siteMetadata.siteUrl + image
-		},
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    >
-		{structuredData ? (
-			Array.isArray(structuredData) 
-			? structuredData.map(
-				(data, index) => <script type="application/ld+json" key={index}>{JSON.stringify(data)}</script>
-				)
-			: <script type="application/ld+json">{JSON.stringify(structuredData)}</script>) : ""}
+	return(
+		<Helmet
+			htmlAttributes={{
+				lang
+			}}
+			title={title}
+			titleTemplate={`%s | ${site.siteMetadata.title}`}
+			link={
+				[
+					{
+						rel: "canonical",
+						href: site.siteMetadata.siteUrl + url
+					},
+					{
+						rel: "alternate",
+						href: "https://chjweb.se" + path,
+						hreflang: "sv"
+					},
+					{
+						rel: "alternate",
+						href: "https://chjweb.se/en" + path,
+						hreflang: "en"
+					}
+				]
+			}
+			meta={[
+				{
+					name: "description",
+					content: metaDescription
+				},
+				{
+					property: "og:title",
+					content: title
+				},
+				{
+					property: "og:description",
+					content: metaDescription
+				},
+				{
+					property: "og:url",
+					content: site.siteMetadata.siteUrl + url
+				},
+				{
+					property: "og:image",
+					content: site.siteMetadata.siteUrl + image
+				},
+				{
+					property: "og:type",
+					content: "website"
+				},
+				{
+					property: "twitter:image",
+					content: site.siteMetadata.siteUrl + image
+				},
+				{
+					name: "twitter:card",
+					content: "summary"
+				},
+				{
+					name: "twitter:creator",
+					content: site.siteMetadata.author
+				},
+				{
+					name: "twitter:title",
+					content: title
+				},
+				{
+					name: "twitter:description",
+					content: metaDescription
+				}
+			].concat(meta)}
+		>
+			{structuredData ? (
+				Array.isArray(structuredData)
+					? structuredData.map(
+						(data, index) => <script type="application/ld+json" key={index}>{JSON.stringify(data)}</script>
+					)
+					: <script type="application/ld+json">{JSON.stringify(structuredData)}</script>) : ""}
 
-		{breadcrumb ? 
-			<script type="application/ld+json">
-				{JSON.stringify({
-					"@context": "https://schema.org",
-					"@type": "BreadcrumbList",
-					"itemListElement": breadcrumb.map((data, index) => (
-						{
-							"@type": "ListItem",
-							"position": index + 1,
-							"name": data.name,
-							"item": `https://chjweb.se${data.url}`
-						}
-					))
-				})}
-			</script>
-			: ""
-		}
-	</Helmet>
-  )
+			{breadcrumb
+				? <script type="application/ld+json">
+					{JSON.stringify({
+						"@context": "https://schema.org",
+						"@type": "BreadcrumbList",
+						"itemListElement": breadcrumb.map((data, index) => (
+							{
+								"@type": "ListItem",
+								"position": index + 1,
+								"name": data.name,
+								"item": `https://chjweb.se${data.url}`
+							}
+						))
+					})}
+				</script>
+				: ""
+			}
+		</Helmet>
+	);
 }
 
 SEO.defaultProps = {
-  lang: `sv`,
-  meta: [],
-  description: ``,
-}
+	lang: "sv",
+	meta: [],
+	description: ""
+};
 
 SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired
-}
+	description: PropTypes.string,
+	lang: PropTypes.string,
+	meta: PropTypes.arrayOf(PropTypes.object),
+	title: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	structuredData: PropTypes.array,
+	breadcrumb: PropTypes.array,
+	image: PropTypes.string
+};
 
-export default SEO
+export default SEO;
